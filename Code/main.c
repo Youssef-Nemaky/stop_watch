84ba5ec77ef70a262c1e9b5544863f8671390d1e
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 20:41:41 +0200

    Set the callback function for timer1

--- Code/main.c
+++ Code/main.c
@@ -30,6 +30,7 @@ void main(void){
     timer1_config_t timer1 = {TIMER_CTC_MODE, T1_F_CPU_64, INITIAL_VALUE_TIMER1, 
     COMPARE_VALUE_TIMER1, LOW, OC1_DISCONNECTED, OC1_DISCONNECTED, 1, LOW};
     
+    TIMER1_setCallBack(increaseTime);
     
     /* SUPER LOOP */
     while(1){
