7f69ea8f845983b15c1590a41642177b7a933fd2
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 20:41:16 +0200

    Add increaseTime function code

--- Code/stop_watch.c
+++ Code/stop_watch.c
@@ -9,3 +9,20 @@
 #include "stop_watch.h"
 
 
+/* This function gets called to increase the time 
+ * It increases the seconds and if overflow occurs (second reaches 60), seconds 
+ * become zero (overflow) and it increases minutes. if minutes becomes 60, overflow occurs
+ * minutes become zero and hours variable is increased if hours become 255 (max value of uint8), 
+ * overflow occurs and hours becomes zero and the stop watch starts again
+ */
+void increaseTime(void){
+    g_time[TIME_SECONDS_INDEX]++;
+    if(g_time[TIME_SECONDS_INDEX] >= MAX_SECONDS_COUNTER){
+        g_time[TIME_SECONDS_INDEX] = LOW;
+        g_time[TIME_MINUTES_INDEX]++;
+        if(g_time[TIME_MINUTES_INDEX] >= MAX_MINUTES_COUNTER){
+            g_time[TIME_MINUTES_INDEX] = LOW;
+            g_time[TIME_HOURS_INDEX]++;
+        }
+    }
+}
