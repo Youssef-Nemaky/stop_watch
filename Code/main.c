90e8ffcca317cf689a78329cf1d1418e01dbb974
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 18:56:50 +0200

    Move main function from stop_watch.c to main.c file

---
+++ Code/main.c
@@ -0,0 +1,19 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 25/10/2021
+* Name: main.c 
+==============================================================================
+*/
+
+void main(void){
+    /* Timer Settings */
+    timer1_config_t timer1 = {TIMER_CTC_MODE, T1_F_CPU_64, INITIAL_VALUE_TIMER1, 
+    COMPARE_VALUE_TIMER1, LOW, OC1_DISCONNECTED, OC1_DISCONNECTED, 1, LOW};
+    
+    
+    /* SUPER LOOP */
+    while(1){
+
+    }
+}
--- Code/stop_watch.c
+++ Code/stop_watch.c
@@ -8,15 +8,4 @@
 
 #include "stop_watch.h"
 
-void main(void){
-    /* Timer Settings */
-    timer1_config_t timer1 = {TIMER_CTC_MODE, T1_F_CPU_64, INITIAL_VALUE_TIMER1, 
-    COMPARE_VALUE_TIMER1, LOW, OC1_DISCONNECTED, OC1_DISCONNECTED, 1, LOW};
-
-    /* SUPER LOOP */
-    while(1){
-
-    }
-}
-
 
