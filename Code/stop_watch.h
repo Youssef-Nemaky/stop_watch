501bb8d40bbb9b571874c39c376f4f53378b7672
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 19:00:09 +0200

    Add a prototype for increaseTime function

--- Code/stop_watch.h
+++ Code/stop_watch.h
@@ -31,4 +31,19 @@
 #define INITIAL_VALUE_TIMER1 (0u)
 #define COMPARE_VALUE_TIMER1 (15625u)
 
+/*
+==============================================================================
+*                            Functions Prototypes
+==============================================================================
+*/
+
+/* This function gets called to increase the time 
+ * It increases the seconds and if overflow occurs (second reaches 60), seconds 
+ * become zero (overflow) and it increases minutes. if minutes becomes 60, overflow occurs
+ * minutes become zero and hours variable is increased if hours become 255 (max value of uint8), 
+ * overflow occurs and hours becomes zero and the stop watch starts again
+ */
+void increaseTime(void);
+
+
 #endif /* STOP_WATCH_H_ */
