795891e0163b6c20ce44bdbf7379d1b7ff9215f3
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 18:47:04 +0200

    Fix a typo in file name comment

--- Code/stop_watch.c
+++ Code/stop_watch.c
@@ -2,7 +2,7 @@
 ==============================================================================
 * Authour: Youssef El-Nemaky
 * Date: 25/10/2021
-* Name: external_interrupt.c 
+* Name: stop_watch.c 
 ==============================================================================
 */
 
@@ -12,7 +12,7 @@ void main(void){
     /* Timer Settings */
     timer1_config_t timer1 = {TIMER_CTC_MODE, T1_F_CPU_64, INITIAL_VALUE_TIMER1, 
     COMPARE_VALUE_TIMER1, LOW, OC1_DISCONNECTED, OC1_DISCONNECTED, 1, LOW};
-    
+
     /* SUPER LOOP */
     while(1){
 
