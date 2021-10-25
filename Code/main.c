bf9809763bdc41b113d6dff03628940bc6048e91
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 20:40:45 +0200

    Add g_time arr that keeps track of time/counting

--- Code/main.c
+++ Code/main.c
@@ -13,6 +13,18 @@
 */
 #include "stop_watch.h"
 
+/*
+==============================================================================
+*                              Global Variables
+==============================================================================
+*/
+/* An Array that contains the time where:
+ * g_time[0] -> seconds
+ * g_time[1] -> minutes
+ * g_time[2] -> hours
+ */
+uint8 g_time[TIME_ARR_LENGTH] = {0};
+
 void main(void){
     /* Timer Settings */
     timer1_config_t timer1 = {TIMER_CTC_MODE, T1_F_CPU_64, INITIAL_VALUE_TIMER1, 
